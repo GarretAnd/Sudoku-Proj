a790840a9d1ad316f3ffab8d7e7691a983fd0293
Quinn A. Spraut 22 <qspraut@flume.cs.dartmouth.edu>
2020-05-27 19:12:01 -0400

    Edits to testing

--- testing.sh
+++ testing.sh
@@ -24,11 +24,27 @@ echo Testing sudoku solve on 'testfile':
 ./sudoku solve < testfile
 echo
 echo Fuzz testing:
-for i in {1..100}
+for i in {1..50}
 do
-	echo Generated Puzzle:
-	./sudoku create > testfile
+	echo Generated Puzzle 1:
+	./sudoku create > testfile1
 	cat testfile
-	echo Solved Puzzle:
-	./sudoku solve < testfile
+	echo Generated Puzzle 2:
+	./sudoku create > testfile2
+	cat testfile1
+	echo Differences in the two puzzles:
+	diff ./"$time" $1 2> /dev/null 1>/dev/null # exit code 0 if no differences, nonzero if differences exist
+	ST=$?
+
+	if [[ $ST -ne 0 ]] # if there are differences
+	then # print the differences and exit
+		echo Differences exist.
+	else # otherwise exit 0
+		echo No differences exist.
+	fi
+	echo Solved Puzzle 1:
+	./sudoku solve < testfile1
+	echo Solved Puzzle 2:
+	./sudoku solve < testfile2
+	
 done
