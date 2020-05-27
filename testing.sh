a9571b1b6537c58e69a1a001ef18801c58dc4855
Quinn A. Spraut 22 <qspraut@flume.cs.dartmouth.edu>
2020-05-27 10:08:44 -0400

    Adding testing (not done) and edits to design

---
+++ testing.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Author: cs50maybemajors
+#Date: 5/27/2020
+#Purpose: CS50 Final Project
+# Tests sudoku.c
+
+echo Testing too few arguments:
+./sudoku
+echo
+echo Testing too many arguments:
+./sudoku create randomword
+echo
+echo Testing invalid argument:
+./sudoku randomword
+echo
+echo Testing functions of libsudoku:
+./common/libsudokutest
+echo
+echo Testing functions of creator:
+./creator/creatortest
+echo
+echo Testing functions of solver:
+./solver/solvertest
+echo
+echo Testing sudoku create, saving into file 'testfile':
+./sudoku create > testfile
+echo
+echo Testing sudoku solve on 'testfile':
+./sudoku solve < testfile
+echo
+echo Fuzz testing:
+
