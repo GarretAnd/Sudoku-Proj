9da64a1863b6cc7403958457eef625d764f0512e
Garret D. Andreine 22 <garretand@moose.cs.dartmouth.edu>
2020-05-29 20:56:44 -0400

    Python Open File

--- graphics/pytest.py
+++ graphics/pytest.py
@@ -1,1 +1,18 @@
-print("hi")
+from cs1lib import *
+
+
+def draw_sudoku(string):
+    file = str(string)
+    board = open(file, "r")  # Takes in file, converts it to a string, and opens it
+
+    y = 0
+    x = 0
+    board_data = []
+
+    for word in board.split():  # Iterates through it and splits it up then puts each one in our board data array
+        board_data[x][y] = word
+        y += 1
+
+        if y == 9:
+            y = 0
+            x += 1
