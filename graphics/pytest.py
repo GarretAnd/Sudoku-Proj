42e6d587fde529fd84e7ceff988c1f5a6ba0e2be
qspraut <63240336+qspraut@users.noreply.github.com>
2020-05-29 20:59:54 -0400

    Update pytest.py

--- graphics/pytest.py
+++ graphics/pytest.py
@@ -16,3 +16,32 @@ def draw_sudoku(string):
         if y == 9:
             y = 0
             x += 1
+
+def run():
+    WIN_X = 360
+    WIN_Y = 360
+    set_clear_color(1, 1, 1)
+    clear()
+
+    i = WIN_X - 40
+    while i > 0:
+        set_stroke_width(1)
+        if i == 120 or i == 240:
+            set_stroke_width(3)
+
+        draw_line(i, WIN_Y, i, 0)
+        draw_line(WIN_X, i, 0, i)
+        i -= 40
+
+    i = 0
+
+    set_font_size(12)
+    while i < 360:
+        j = 0
+        while j < 360:
+            draw_text("0", i + 15, j + 30)
+            j += 40
+        i += 40
+
+
+start_graphics(run, framerate=100, width=360, height=360)
